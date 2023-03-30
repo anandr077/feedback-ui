f169a0ab726ee38cb82b490c4307b66c5ce2ff6d
Aditya Chondke <aditya.chondke@gmail.com>
2023-03-30 09:30:59 +0530

    added navbar funcation

--- components/HeaderSmall/index.tsx
+++ components/HeaderSmall/index.tsx
@@ -1,16 +1,37 @@
 import React from "react";
 import styled from "styled-components";
+import MenuMobile from "../MenuMobile";
 import Notifications from "../Notifications";
 
 export default function HeaderSmall() {
+  const [isNotificationOpen, setIsNotificationOpen] = React.useState(false);
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const handleNotificationClick = () => {
+    setIsNotificationOpen(!isNotificationOpen);
+  };
+  const handleMenuClick = () => {
+    setIsMenuOpen(!isMenuOpen);
+    console.log(isMenuOpen);
+  };
+
   return (
-    <Frame1350>
-      <Frame1349 src="/img/frame-1349-1.png" />
-      <Frame5>
-        <Notifications src="/img/notificationbing@2x.png" />
-        <Frame51 src="/img/frame-5@2x.png" />
-      </Frame5>
-    </Frame1350>
+    <>
+      <Frame1350>
+        <Frame1349 src="/img/frame-1349-1.png" />
+        <Frame5>
+          <Notifications src="/img/notificationbing@2x.png" />
+          <Frame51 src="/img/frame-5@2x.png" onClick={handleMenuClick} />
+        </Frame5>
+
+        {isMenuOpen && (
+          <Popup>
+            {" "}
+            <MenuMobile />{" "}
+          </Popup>
+        )}
+      </Frame1350>
+    </>
   );
 }
 
@@ -21,6 +42,7 @@ const Frame1350 = styled.div`
   padding: 16px 20px;
   position: relative;
   width: 100%;
+  max-height: 70px;
   background-color: var(--white);
 `;
 
@@ -46,4 +68,13 @@ const Frame51 = styled.img`
   min-width: 48px;
   height: 48px;
   cursor: pointer;
+`;
+
+const Popup = styled.div`
+  position: absolute;
+  right: 10px;
+  top: 70px;
+  height: 100px;
+  cursor: pointer;
+  z-index: 1;
 `;

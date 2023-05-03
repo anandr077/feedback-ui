5b6e180df512e3e01c992affb4e2c3eb2a712593
Aman Manocha <amanmanocha@yahoo.com>
2023-05-03 19:11:27 +1000

    Show snackbar

--- components/ReactiveRender/index.jsx
+++ components/ReactiveRender/index.jsx
@@ -2,21 +2,47 @@ import React from "react";
 import { useMediaQuery } from "react-responsive";
 import SnackbarContext from '../SnackbarContext';
 import { Snackbar } from '@mui/material';
+import Button from '@mui/material/Button';
+import Snackbar from '@mui/material/Snackbar';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
 export default function ReactiveRender(props) {
-  const { snackbarOpen, snackbarMessage, closeSnackbar } = React.useContext(SnackbarContext);
+  const { snackbarOpen, snackbarMessage, snackbarLink, closeSnackbar } = React.useContext(SnackbarContext);
 
   const { mobile, tablet, laptop, desktop } = props;
   const isMobileView = useMediaQuery({ maxWidth: 1023 });
   const isTabletView = useMediaQuery({ minWidth: 1024, maxWidth: 1439 });
   const isLaptopView = useMediaQuery({ minWidth: 1440, maxWidth: 1919 });
   const isDesktopView = useMediaQuery({ minWidth: 1920 });
+  const linkButton = snackbarLink ? <Button color="secondary" size="small" 
+  onClick={()=>{
+    window.location.href = snackbarLink
+    closeSnackbar()
+  }}>
+    View
+  </Button>:<></>
+  const action = (
+    <React.Fragment>
+      {linkButton}
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={closeSnackbar}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </React.Fragment>
+  );
+ 
   return (
     <>
       <Snackbar
     open={snackbarOpen}
     message={snackbarMessage}
     onClose={closeSnackbar}
-    autoHideDuration={3000}
+    autoHideDuration={5000}
+    action={action}
   />
       {isMobileView && mobile}
       {isTabletView && tablet}

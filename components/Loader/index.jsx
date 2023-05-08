d0071e50d0d33ca2f84eb93cd99a1553cf412f53
Aman Manocha <amanmanocha@yahoo.com>
2023-05-09 08:36:06 +1000

    Smaller loader

--- components/Loader/index.jsx
+++ components/Loader/index.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import Lottie from "lottie-react";
 import animationData from "./data.json";
-import "@lottiefiles/lottie-player";
 
 export default function Loader() {
-  return <lottie-player
-  autoplay
-  loop
-  mode="normal"
-  src="./icons/data.json"
-  style={{ width: "100vw", height: "100vh" }}
->
-</lottie-player>;
+  return (
+    <div style={{ position: "fixed", top: 0, bottom: 0, left: 0, right: 0, display: "flex", justifyContent: "center", alignItems: "center" }}>
+      <div style={{ width: "20%", maxWidth: "100px", height: "auto" }}>
+        <Lottie animationData={animationData} autoplay loop />
+      </div>
+    </div>
+  );
 }

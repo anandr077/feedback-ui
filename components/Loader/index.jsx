c399fbf2475cc1cffb53f96ea5515feadba196f6
Aman Manocha <amanmanocha@yahoo.com>
2023-05-09 11:07:48 +1000

    Loader in center

--- components/Loader/index.jsx
+++ components/Loader/index.jsx
@@ -1,48 +1,53 @@
 import React from "react";
-import Lottie from "lottie-react";
-import animationData from "./data.json";
 import styled from "styled-components";
 import "@lottiefiles/lottie-player";
 
 export default function Loader() {
-  return<Container> 
-   <LottieContainer><lottie-player
-  autoplay
-  loop
-  mode="normal"
-  src="./icons/data.json"
->
-</lottie-player>
-</LottieContainer> 
-<StyledText>Making sure everything looks good...</StyledText>
-</Container>;
+  return (
+    <Container>
+      <LottieContainer>
+        <lottie-player
+          autoplay
+          loop
+          mode="normal"
+          src="./icons/data.json"
+        ></lottie-player>
+      </LottieContainer>
+      <StyledText>Making sure everything looks good...</StyledText>
+    </Container>
+  );
 }
+
 const Container = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
-top : 30%;
-z-index: 1;
-position: fixed;
-left: 760px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: 1;
 `;
+
 const LottieContainer = styled.div`
-display: flex;
-align-items: center;
-padding: 0px;
-z-index: 1;
-width: 326px;
-height: 270px;
-;`;
+  display: flex;
+  align-items: center;
+  padding: 0px;
+  z-index: 1;
+  width: auto;
+  height: 100px;
+`;
 
 const StyledText = styled.div`
-width: 326px;
-height: 26px;
-font-family: '"IBM Plex Sans", sans-serif';
-font-style: normal;
-font-weight: 400;
-font-size: 15px;
-line-height: 26px;
-text-align: center;
-color: #1E252A;
+  width: 326px;
+  height: 26px;
+  font-family: "IBM Plex Sans", sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 15px;
+  line-height: 26px;
+  text-align: center;
+  color: #1e252a;
 `;

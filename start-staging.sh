7f923afe5b3ef7c9e04bbce1e9a01ba003594e83
Aman Manocha <amanmanocha@yahoo.com>
2023-05-28 12:10:48 +1000

    Setup env

--- start-staging.sh
+++ start-staging.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-npm install --save-dev parcel-bundler
-npm install --save-dev env-cmd
-npm run build:staging
-npm run serve:staging
+export REACT_APP_API_BASE_URL=https://feedbacks-backend-leso2wocda-ts.a.run.app
+export REACT_APP_JEDDLE_BASE_URL=https://jeddle.duxdigital.net
+export REACT_APP_SELF_BASE_URL=https://feedbackuianimaap-leso2wocda-ts.a.run.app
+export SELF_BASE_URL=https://feedbackuianimaap-leso2wocda-ts.a.run.app
+export REACT_APP_CLIENT_ID=glkjMYDxtVbCbGabAyuxfMLJkeqjqHyr
+
+npm run serve

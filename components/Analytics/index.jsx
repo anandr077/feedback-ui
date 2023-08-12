d0bab53f4627cf3425092a11f7e252a9976c9546
Aditya Chondke <aditya.chondke@gmail.com>
2023-08-12 16:24:06 +0530

    ui for subcategories

--- components/Analytics/index.jsx
+++ components/Analytics/index.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React , {useEffect, useState} from "react";
 import { Chart, ArcElement } from "chart.js";
 import { Pie } from "react-chartjs-2";
 import "./index.css";
 import ProgressBar from "../ProgressBar";
 import randomColor from "randomcolor";
+import SmartAnotationAnalytics from "../SmartAnnotationsAnalytics";
+
 
 Chart.register(ArcElement);
 
 export default function AnnotationAnalytics(props) {
   const {smartAnnotationAnalytics} = props;
+ 
+
   let data = [];
   let labels = [];
 
@@ -19,7 +23,19 @@ export default function AnnotationAnalytics(props) {
     }
     );
     data.push(sum);
-    labels.push(key);
+  labels.push(key);
+});
+
+
+const total = data.reduce((sum, value) => sum + value, 0);
+const percentages = data.map(num => ((num / total) * 100).toFixed(2));
+
+const smartAnnotationAnalyticsData = [];
+let count = 0;
+smartAnnotationAnalytics.forEach((element, key) => {
+  const jsxElement = <SmartAnotationAnalytics title={key} childrens={element} total= {data[count]} />;
+  smartAnnotationAnalyticsData.push(jsxElement);
+  count++;
 });
 
 
@@ -28,12 +44,11 @@ export default function AnnotationAnalytics(props) {
     datasets: [
       {
         backgroundColor: randomColor({ count: data.length }),
-        data: data,
+        data: percentages,
       },
     ],
   };
-  const total = data.reduce((sum, value) => sum + value, 0);
-
+ 
   const legendItems = labels.map((label, index) => (
     <div key={index} className="legend-item">
       <div
@@ -42,7 +57,7 @@ export default function AnnotationAnalytics(props) {
           backgroundColor: chartData.datasets[0].backgroundColor[index],
         }}
       ></div>
-      <div className="legend-label">{`${label}: ${data[index]}`}</div>
+      <div className="legend-label">{`${label}: ${percentages[index]}%`}</div>
     </div>
   ));
 
@@ -60,12 +75,13 @@ export default function AnnotationAnalytics(props) {
             </div>
             <div className="legend">
               {legendItems}
-              <div className="legend-label">{`Total: ${total}`}</div>
+              {/* <div className="legend-label">{`Total: ${total}`}</div> */}
             </div>
           </div>
         </div>
         <div className="progress">
-          <ProgressBar title={"Total"} count={10} total={20} />
+{smartAnnotationAnalyticsData}
+          {/* <ProgressBar title={"Total"} count={10} total={20} /> */}
         </div>
       </div>
     </>

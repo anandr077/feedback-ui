7437e80fd24213d61be8e2c550d4d4b8bf0be229
Aditya Chondke <aditya.chondke@gmail.com>
2023-08-16 15:43:21 +0530

    stats inside category in percentages nnow

--- components/ProgressBar/index.jsx
+++ components/ProgressBar/index.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import "./index.css";
 
 export default function ProgressBar({ title, count, total }) {
-  const percentage = count <= 0 ? 0 : (count/total) * 100;
+  const percentage = count <= 0 ? 0 : (((count/total) * 100).toFixed(2));
   console.log("percentage: ", percentage);
   return (
     <div className="progress-bar-container">
       <div className="progress-bar-heading">
         <div className="title">{title}</div>
-        <div className="title">{count}</div>
+        <div className="title">{percentage}%</div>
       </div>
       <div class="progress-container">
         <div class="progress-bar" style={{ width: `${percentage}%` }}></div>

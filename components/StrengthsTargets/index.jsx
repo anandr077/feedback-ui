a01b75f4f4639df80402074975a92307c3d233e4
Mahmudul Hasan <mhasan.bd.95@gmail.com>
2023-09-04 01:25:42 +0600

    added the strengths and target component

---
+++ components/StrengthsTargets/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const strengths = [
+    {
+        title: 'Engagement with the question',
+        item: 'You start your paragraphs with a point sentence that links to the question'
+    },
+    {
+        title: 'Use of evidence to support argument',
+        item: 'Introduces evidence and/or supporting ideas in relation to the [concept / postion / question]'
+    },
+]
+
+const targets = [
+    {
+        title: 'Engagement with the question',
+        item: 'Begin your paragraphs with a point sentence that links to the question' 
+    }
+]
+
+const StrengthsTargets = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default StrengthsTargets

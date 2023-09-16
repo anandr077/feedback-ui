9e3790dde5b3988c0a68d70dc06198c4756db8bc
Yogesh <yogesh@pureblocks.com.au>
2023-09-16 15:18:38 +0530

    refactor Start Assignment resolution

--- components/StartAssignment/TaskDetailTablet/index.jsx
+++ components/StartAssignment/TaskDetailTablet/index.jsx
@@ -7,38 +7,16 @@ import Breadcrumb from '../../Breadcrumb';
 import Breadcrumb2 from '../../Breadcrumb2';
 import Buttons from '../Buttons';
 import Frame11 from '../Frame11';
-import Frame12093 from '../Frame12093';
 import GoBack from '../GoBack';
 import StatusBubbles from '../StatusBubbles';
 import {
   IbmplexsansBoldShark36px,
-  IbmplexsansNormalChicago13px,
-  IbmplexsansNormalPersianIndigo13px,
   IbmplexsansSemiBoldShark28px,
 } from '../styledMixins';
 import './TaskDetailTablet.css';
 import FooterSmall from '../../FooterSmall';
 function TaskDetailTablet(props) {
-  const {
-    assignment,
-    methods,
-    clazzName,
-    mobile,
-    frame1349,
-    frame5,
-    title,
-    physicsThermodyna,
-    line11,
-    x2023JeddleAllRightsReserved,
-    mainWebsite,
-    terms,
-    privacy,
-    breadcrumb21Props,
-    breadcrumb22Props,
-    goBackProps,
-    frame120931Props,
-    frame120932Props,
-  } = props;
+  const { assignment, methods, clazzName, line11, goBackProps } = props;
 
   return (
     <div className="task-detail-tablet screen">
@@ -62,10 +40,6 @@ function TaskDetailTablet(props) {
             <Frame28>
               <Frame11 text={assignment.questions.length} />
             </Frame28>
-            {/* <Frame12093
-              topicsCovered={frame120932Props.topicsCovered}
-              moremIpsumDolorSi={frame120932Props.moremIpsumDolorSi}
-            /> */}
             <Frame1209>
               <Line11 src={line11} alt="Line 11" />
               <Buttons onClickFn={methods.onClickStartAssignment} />
@@ -74,16 +48,6 @@ function TaskDetailTablet(props) {
         </Frame1390>
       </Frame1391>
       <FooterSmall />
-      {/* <Frame1392>
-        <X2023JeddleAllRightsReserved>
-          {x2023JeddleAllRightsReserved}
-        </X2023JeddleAllRightsReserved>
-        <Frame6>
-          <MainWebsite>{mainWebsite}</MainWebsite>
-          <Terms>{terms}</Terms>
-          <Terms>{privacy}</Terms>
-        </Frame6>
-      </Frame1392> */}
     </div>
   );
 }
@@ -95,40 +59,6 @@ const Frame1391 = styled.div`
   gap: 20px;
   position: relative;
   align-self: stretch;
-`;
-
-const Frame1350 = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 20px;
-  padding: 16px 20px;
-  position: relative;
-  align-self: stretch;
-  background-color: var(--white);
-`;
-
-const Frame1349 = styled.img`
-  position: relative;
-  flex: 1;
-  min-width: 857.75px;
-  height: 37.48848342895508px;
-  margin-left: -1.75px;
-`;
-
-const Frame5 = styled.div`
-  display: flex;
-  width: fit-content;
-  align-items: center;
-  justify-content: flex-end;
-  gap: 12px;
-  position: relative;
-`;
-
-const Frame51 = styled.img`
-  position: relative;
-  min-width: 48px;
-  height: 48px;
-  cursor: pointer;
 `;
 
 const Frame1390 = styled.div`
@@ -229,17 +159,6 @@ const Frame1209 = styled.div`
   align-self: stretch;
 `;
 
-const Frame6 = styled.div`
-  ${IbmplexsansNormalPersianIndigo13px}
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 20px;
-  position: relative;
-  align-self: stretch;
-`;
-
 const Line11 = styled.img`
   position: relative;
   align-self: stretch;
@@ -247,44 +166,6 @@ const Line11 = styled.img`
   height: 1.00006103515625px;
   margin-top: -1px;
   object-fit: cover;
-`;
-
-const Frame1392 = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 20px;
-  padding: 40px 20px;
-  position: relative;
-  align-self: stretch;
-  background-color: var(--white);
-`;
-
-const X2023JeddleAllRightsReserved = styled.p`
-  ${IbmplexsansNormalChicago13px}
-  position: relative;
-  align-self: stretch;
-  margin-top: -1px;
-  text-align: center;
-  letter-spacing: 0;
-  line-height: normal;
-`;
-
-const MainWebsite = styled.div`
-  position: relative;
-  align-self: stretch;
-  margin-top: -1px;
-  text-align: center;
-  letter-spacing: 0;
-  line-height: normal;
-`;
-
-const Terms = styled.div`
-  position: relative;
-  align-self: stretch;
-  text-align: center;
-  letter-spacing: 0;
-  line-height: normal;
 `;
 
 export default TaskDetailTablet;

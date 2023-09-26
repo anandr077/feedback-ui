0cde9dceaba1d6f34c1d28407e2759460b288394
Yogesh <yogesh@pureblocks.com.au>
2023-09-26 22:14:55 +0530

    refactor breadcrumb

---
+++ components/Breadcrumb/style.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+import { IbmplexsansNormalFuscousGray14px } from '../../styledMixins';
+
+export const Breadcrumb1 = styled.article`
+  display: flex;
+  width: fit-content;
+  align-items: center;
+  gap: 4px;
+  position: relative;
+  cursor: pointer;
+`;
+
+export const Assignments = styled.div`
+  ${IbmplexsansNormalFuscousGray14px}
+  position: relative;
+  width: fit-content;
+  margin-top: -1px;
+  letter-spacing: 0;
+  line-height: normal;
+`;

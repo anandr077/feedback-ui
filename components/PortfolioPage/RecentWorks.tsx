6daea51a9a2eba1b260092cc69bb3dfb91bd9f0e
Mahmudul Hasan <mahmudul@pureblocks.com.au>
2023-10-03 09:00:06 +0600

    removed the dropdown from ther protfolio header and checked the all file and recent file words limit

--- components/PortfolioPage/RecentWorks.tsx
+++ components/PortfolioPage/RecentWorks.tsx
@@ -13,7 +13,7 @@ import { downloadPortfolioPdf } from '../Shared/helper/downloadPdf';
 const RecentWorks = ({ work }) => {
   return (
     <RecentWork>
-      <RecentWorkPara>{work?.preview?.slice(0, 400)}</RecentWorkPara>
+      <RecentWorkPara>{work?.preview?.slice(0, 380)}</RecentWorkPara>
       <RecentWorkTitle>{work.title}</RecentWorkTitle>
       <div className="recent-hover">
         <a href={work.url} style={{ textDecoration: 'none' }}>

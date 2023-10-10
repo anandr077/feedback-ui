150b21555588c7eed3f5b67af2f32086669cea26
Yogesh <yogesh@pureblocks.com.au>
2023-10-10 21:13:05 +0530

    fix document mobile screen rendring

--- components/FeedbacksComponents/Document/index.jsx
+++ components/FeedbacksComponents/Document/index.jsx
@@ -118,7 +118,10 @@ function Document(props) {
   };
   return (
     <>
-      <div className="feedback-teacher-laptop screen">
+      <div
+        className="feedback-teacher-laptop screen"
+        style={{ minWidth: 'unset' }}
+      >
         <Frame1388>
           {breadcrumbs(pageMode, submission)}
           {answersAndFeedbacks(
@@ -329,8 +332,14 @@ function breadcrumbs(pageMode, submission) {
       <Frame1387>
         <Frame1315>
           <Breadcrumb text={'Portfolio'} link={'/#/portfolio'} />
-          <Breadcrumb2 assignments={'Class' + submission.classId} link={'/#/portfolio/'+submission.classId}/>
-          <Breadcrumb2 assignments={'Drafts'} link={'/#/portfolio/'+submission.classId+'/Drafts'}/>
+          <Breadcrumb2
+            assignments={'Class' + submission.classId}
+            link={'/#/portfolio/' + submission.classId}
+          />
+          <Breadcrumb2
+            assignments={'Drafts'}
+            link={'/#/portfolio/' + submission.classId + '/Drafts'}
+          />
 
           <Breadcrumb2 assignments={submission.assignment.title} />
         </Frame1315>

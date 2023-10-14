42da6e61016798d7372235f6ce0bf229ab2e370c
Mahmudul Hasan <mahmudul@pureblocks.com.au>
2023-10-14 19:36:46 +0600

    sidebar folder is deletable and new to old will be by default

--- components/PortfolioPage/PortfolioSidebarFolder.tsx
+++ components/PortfolioPage/PortfolioSidebarFolder.tsx
@@ -25,6 +25,7 @@ const PortfolioSidebarFolder = ({
   setActiveFolderIndex,
   clickedSubfolder,
   setClickedSubfolder,
+  handleFolderDelete
 }) => {
   const isActive = isMobileView() ? showNavMenu : true;
   const [activeDropdown, setActiveDropdown] = useState(false);
@@ -61,6 +62,7 @@ const PortfolioSidebarFolder = ({
   const handleDeleteDropdown = (event) => {
     setActiveDropdown((prev) => !prev);
     event.stopPropagation();
+    handleFolderDelete(folder.id)
   };
 
 
@@ -83,7 +85,7 @@ const PortfolioSidebarFolder = ({
             );
           }}
           to={`/portfolio/` + folder.id + '/' + (selectedSubFolder || '')}
-          title={folder.title.length > 12 ? folder.title : undefined}
+          title={folder.title?.length > 12 ? folder.title : undefined}
         >
           {isEditing ? (
             <input
@@ -97,7 +99,7 @@ const PortfolioSidebarFolder = ({
                 }
               }}
             />
-          ) : folder.title.length > 12 ? (
+          ) : folder.title?.length > 12 ? (
             `${folder.title.slice(0, 12)}...`
           ) : (
             folder.title
@@ -113,11 +115,15 @@ const PortfolioSidebarFolder = ({
                       }`}
                     >
                       <span onClick={handleEditDropdown}>
-                        <EditNoteIcon /> Rename
+                            <EditNoteIcon /> Rename
                       </span>
-                      <span onClick={handleDeleteDropdown}>
-                        <img src={deleteIcon} /> Delete
-                      </span>
+                      {
+                        folder.allowDelete && (
+                          <span onClick={handleDeleteDropdown}>
+                            <img src={deleteIcon} /> Delete
+                          </span>
+                        )
+                      }
                     </div>
                   </div>
                 )}

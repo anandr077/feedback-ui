ad64884fef72480729ce4a78d274b2a821611371
Aman Manocha <amanmanocha@yahoo.com>
2023-11-01 02:20:52 +1100

    Portfolio notifications

--- components/TeacherDashboard/TeacherDashboardDesktop/index.jsx
+++ components/TeacherDashboard/TeacherDashboardDesktop/index.jsx
@@ -102,8 +102,8 @@ function recentActivitiesFrame(notifications) {
       link: notification.link,
       title: notification.title,
       classTitle:
-        notification.classTitle +
-        ' | at ' +
+      (notification.classTitle ? (notification.classTitle +
+        ' | at '):'') +
         timeFirstFormattedDate(notification.dueAt),
     };
     return <TaskCard task={task} />;

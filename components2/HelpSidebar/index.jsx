313e7cdc41e0a40a0a90b46b9b5d7b49acc22c60
Mahmudul Hasan <mahmudul@pureblocks.com.au>
2024-01-02 10:19:23 +0600

    added search feature to the helpbar

--- components2/HelpSidebar/index.jsx
+++ components2/HelpSidebar/index.jsx
@@ -9,33 +9,49 @@ import {
   MoreOption,
   MoreOptionIcon,
   HelpSidebarSmallContainer,
-  CloseHelpBar
+  CloseHelpBar,
 } from './style';
 import Accordion from './Accordion';
 import { isSmallScreen } from '../../components/ReactiveRender';
-import helpdata from './helpdata.json'
+import helpdata from './helpdata.json';
 
-
-const HelpSidebar = ({onCloseFn}) => {
+const HelpSidebar = ({ onCloseFn }) => {
   const isSmallView = isSmallScreen();
-  const [data, setData] = useState(helpdata.data)
+  const [data, setData] = useState(helpdata.data);
+  const [searchQuery, setSearchQuery] = useState('');
 
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+    if (!query) {
+      setData(helpdata.data);
+      return;
+    }
+
+    const filteredData = helpdata.data.filter(section => 
+      section.title.toLowerCase().includes(query.toLowerCase()) ||
+      section.subtopics.some(subtopic => 
+        subtopic.title.toLowerCase().includes(query.toLowerCase())
+      )
+    );
+
+    setData(filteredData);
+  };
 
   return isSmallView ? (
     <HelpSidebarSmallContainer>
-      <CloseHelpBar src="/img/close.png" onClick={onCloseFn}/>
-      {helpSidebarContent(data)}
+      <CloseHelpBar src="/img/close.png" onClick={onCloseFn} />
+      {helpSidebarContent(data, handleSearch)}
     </HelpSidebarSmallContainer>
   ) : (
     <HelpSidebarContainer onClick={(e) => e.stopPropagation()}>
-      {helpSidebarContent(data)}
+      {helpSidebarContent(data, handleSearch)}
     </HelpSidebarContainer>
   );
 };
 
 export default HelpSidebar;
 
-function helpSidebarContent(data) {
+function helpSidebarContent(data, handleSearch) {
   return (
     <>
       <Header>
@@ -43,7 +59,10 @@ function helpSidebarContent(data) {
         Help Centre
       </Header>
       <SearchContainer>
-        <Input placeholder="Search for topics" />
+        <Input
+          placeholder="Search for topics"
+          onChange={(e) => handleSearch(e.target.value)}
+        />
         <SearchIcon src="/img/find-replace.png" />
       </SearchContainer>
       {data.map((section, index) => (

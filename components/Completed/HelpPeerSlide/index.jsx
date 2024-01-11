3d542e654e565346c7923ea7d70945b847d16363
Mahmudul Hasan <mahmudul@pureblocks.com.au>
2024-01-11 15:03:19 +0600

    added slide to the exemplarResponse

--- components/Completed/HelpPeerSlide/index.jsx
+++ components/Completed/HelpPeerSlide/index.jsx
@@ -1,25 +1,90 @@
 import React, { useState } from 'react';
-import { SliderContainer, Header, SliderMain, Slider, Slides } from './style';
+import {
+  SliderContainer,
+  SlideContainer,
+  Header,
+  SliderMain,
+  Slider,
+  FirstSlide,
+  SecondSlide,
+  ThirdSlide,
+  SlideDots,
+  Dot,
+} from './style';
 import TaskCard from '../../TaskCard';
 
 const HelpPeerSlide = (props) => {
   const { id, groups, exemplar, setPublishActionCompleted } = props;
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [animateSlide, setAnimateSlide] = useState(false);
 
   const requireAction = Object.values(groups)
     .flat()
     .filter((task) => task.status === 'AWAITING_APPROVAL');
 
-  const tasksFrames = requireAction.map((task, idx) => (
-    <Slides>
-      <TaskCard
-        key={task.id}
-        task={task}
-        exemplar={exemplar}
-        isSelected={task.id === props.id}
-        setPublishActionCompleted={setPublishActionCompleted}
-      />
-    </Slides>
-  ));
+  const handleSlideChange = () => {
+    setAnimateSlide(true);
+
+    setTimeout(() => {
+      const nextSlideIndex = currentSlide + 1;
+      if (nextSlideIndex < requireAction.length) {
+        setCurrentSlide(nextSlideIndex);
+      } else {
+        setCurrentSlide(requireAction.length);
+      }
+      setAnimateSlide(false);
+    }, 300);
+  };
+
+  const tasksFrames = requireAction.map((task, idx) => {
+    if (idx === currentSlide) {
+      return (
+        <SlideContainer key={task.id}>
+          {idx === currentSlide && idx < requireAction.length - 1 && (
+            <SecondSlide key={`next_${requireAction[idx + 1].id}`}>
+              <div
+                style={{
+                  width: '100%',
+                  height: '100%',
+                  top: animateSlide && '30px',
+                }}
+              >
+                <TaskCard
+                  key={requireAction[idx + 1].id}
+                  task={requireAction[idx + 1]}
+                  exemplar={exemplar}
+                  isSelected={requireAction[idx + 1].id === props.id}
+                  setPublishActionCompleted={setPublishActionCompleted}
+                />
+              </div>
+            </SecondSlide>
+          )}
+          <FirstSlide
+            animate={animateSlide}
+            style={{
+              visibility: 'visible',
+            }}
+          >
+            <div style={{ width: '100%', height: '100%' }}>
+              <TaskCard
+                key={task.id}
+                task={task}
+                exemplar={exemplar}
+                isSelected={task.id === props.id}
+                setPublishActionCompleted={setPublishActionCompleted}
+                onSlideChange={handleSlideChange}
+              />
+            </div>
+          </FirstSlide>
+          <ThirdSlide></ThirdSlide>
+        </SlideContainer>
+      );
+    }
+  });
+
+  if (currentSlide === requireAction.length - 1 || requireAction.length === 0) {
+    return <></>;
+  }
 
   return (
     <SliderContainer>
@@ -31,6 +96,14 @@ const HelpPeerSlide = (props) => {
       <SliderMain>
         <Slider>{tasksFrames}</Slider>
       </SliderMain>
+      <SlideDots>
+        {currentSlide < requireAction.length - 1 &&
+          requireAction
+            .slice(0, -1)
+            .map((task, index) => (
+              <Dot key={task.id} isActive={index === currentSlide} />
+            ))}
+      </SlideDots>
     </SliderContainer>
   );
 };
--- components/Completed/HelpPeerSlide/style.js
+++ components/Completed/HelpPeerSlide/style.js
@@ -25,11 +25,70 @@ export const Header = styled.div`
 
 export const SliderMain = styled.div`
   padding: 20px;
+  height: 430px;
+  overflow: hidden;
 `;
 
 export const Slider = styled.div`
   position: relative;
   width: 100%;
+  height: 100%;
 `;
 
-export const Slides = styled.div``;
+export const SlideContainer = styled.div`
+  position: relative;
+  height: 100%;
+`;
+
+export const FirstSlide = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 70%;
+  top: 30px;
+  transition: transform 0.3s ease-in;
+  transform: translateX(${(props) => (props.animate ? '100%' : '0')});
+  z-index: 2;
+`;
+
+export const SecondSlide = styled.div`
+  position: absolute;
+  top: 15px;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 97%;
+  background-color: var(--white);
+  height: 95%;
+  z-index: 1;
+  border: 1px solid rgba(114, 0, 224, 0.1);
+  border-radius: 15px;
+  box-shadow: 0px 3.826px 15.304px 0px rgba(114, 0, 224, 0.1);
+`;
+
+export const ThirdSlide = styled.div`
+  position: absolute;
+  top: 0px;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 94%;
+  height: 95%;
+  z-index: 0;
+  border: 1px solid rgba(114, 0, 224, 0.1);
+  border-radius: 15px;
+  background-color: var(--white);
+  box-shadow: 0px 3.826px 15.304px 0px rgba(114, 0, 224, 0.1);
+`;
+
+export const SlideDots = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 8px;
+  margin-bottom: 20px;
+`;
+
+export const Dot = styled.div`
+  background-color: ${(props) =>
+    props.isActive ? 'var(--light-mode-purple)' : '#D6D6D6'};
+  width: 8px;
+  height: 8px;
+  border-radius: 50%;
+`;

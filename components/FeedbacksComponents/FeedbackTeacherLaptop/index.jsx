a18a65c2b964f841a06e158d68bd1ea68f91fbf8
Mahmudul Hasan <mahmudul@pureblocks.com.au>
2024-02-27 13:48:14 +0600

    fixed the sidebar issue

--- components/FeedbacksComponents/FeedbackTeacherLaptop/index.jsx
+++ components/FeedbacksComponents/FeedbackTeacherLaptop/index.jsx
@@ -3,6 +3,7 @@ import 'quill/dist/quill.core.css';
 import 'quill/dist/quill.snow.css';
 import { default as React, default as React, useEffect, useState, useContext } from 'react';
 import Header from '../../Header';
+import { getUserRole } from '../../../userLocalDetails'; 
 
 import { flatMap, groupBy } from 'lodash';
 import HeaderSmall from '../../HeaderSmall';
@@ -43,7 +44,7 @@ import TeacherSidebar from '../../TeacherSidebar';
 import IndepentdentUserSidebar from '../../IndependentUser/IndepentdentUserSidebar';
 import { FeedbackContext } from '../FeedbacksRoot/FeedbackContext';
 
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 import FeedbackTypeDialog from '../../Shared/Dialogs/feedbackType';
 import { getSubmissionById, createRequestFeddbackType } from '../../../service';
 import StyledDropDown from '../../../components2/StyledDropDown';
@@ -102,7 +103,7 @@ function FeedbackTeacherLaptop(props) {
   const [groupedFocusAreaIds, setGroupedFocusAreaIds] = React.useState(() =>
     createGroupedFocusAreas(submission)
   );
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
   const [groupedAndSortedData, setGroupedAndSortedData] = React.useState({});
   const [selectedSubject, setSelectedSubject] = React.useState();
   const [sidebarCollapsed, setSidebarCollapsed] = React.useState(false);
@@ -139,6 +140,7 @@ function FeedbackTeacherLaptop(props) {
     setSelectedSubject(Object.keys(groupedData)[0]);
   }, [submission.otherDrafts]);
   const navigate = useHistory();
+  const location = useLocation();
 
   const [showStudentPopUp, setShowStudentPopUp] = React.useState(false);
   const [showTeacherPopUp, setShowTeacherPopUp] = React.useState(false);
@@ -214,6 +216,16 @@ function FeedbackTeacherLaptop(props) {
       quillRef?.redrawHighlights(currentTabComments);
     }
   }, [showNewComment]);
+
+  React.useEffect(() => {
+    const documentsRoute = location.pathname.includes('documents'); 
+    const submissionsRoute = location.pathname.includes('submissions');
+    const role = getUserRole()
+
+    const isOpen = (role === 'TEACHER' && submissionsRoute) || (role === 'STUDENT' && documentsRoute);
+    setOpen(isOpen)
+  }, [location.pathname]); 
+
   const handleCheckboxChange = (serialNumber, focusAreaId) => (event) => {
     const isChecked = event.target.checked;
     setGroupedFocusAreaIds((prevState) => {

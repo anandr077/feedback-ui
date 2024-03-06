6892581aee52c55f157408271fc84f3a315220d3
Mahmudul Hasan <mahmudul@pureblocks.com.au>
2024-03-06 18:37:19 +0600

    added notification and account

--- components/Header2/HeaderStyle.js
+++ components/Header2/HeaderStyle.js
@@ -1,3 +1,4 @@
+import styled, { keyframes } from 'styled-components';
 import styled from "styled-components";
 
 export const MainContainer = styled.div`
@@ -54,4 +55,88 @@ export const Account = styled.img`
    height: 36px;
    width: 36px;
    cursor: pointer;
+`;
+
+
+export const Screen = styled.div`
+  height: ${(props) =>
+    props.pageHeight ? props.pageHeight + 170 + 'px' : '0'};
+  //height: 100%;
+  width: calc(100vw - 20px);
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 10;
+`;
+
+
+const slideIn = keyframes`
+   0%{
+    right: -400px;
+   }
+   25%{
+    right: -300px;
+   }
+   50%{
+    right: -200px;
+   }
+   75%{
+    right: -100px;
+   }
+   100%{
+    right: 0
+   }
+`;
+
+const slideOut = keyframes`
+  0% {
+    right: 0;
+  }
+  25% {
+    right: -100px;
+  }
+  50% {
+    right: -200px;
+  }
+  75% {
+    right: -300px;
+  }
+  100% {
+    right: -400px;
+  }
+`;
+
+export const NavigationContainer = styled.div`
+  position: absolute;
+  top: 70px;
+  right: 0;
+  z-index: 10000 !important;
+  border-radius: 8px;
+  background-color: white;
+  align-self: stretch;
+  overflow-y: scroll;
+  padding-bottom: 15px;
+  box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.1);
+  height: ${(props) => (props.pageHeight ? props.pageHeight + 'px' : '0')};
+  animation: ${(props) => (props.slideNotificationBar ? slideIn : slideOut)}
+    0.3s linear forwards;
+  &::-webkit-scrollbar {
+    width: 0;
+    display: none;
+  }
+`;
+
+
+export const DropDownContainer = styled.div`
+  position: absolute;
+  top: 70px;
+  right: 30px;
+  z-index: 1;
+  background-color: var(--white);
+  border-radius: 8px;
+  box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.1);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 5px 2px 5px 2px;
 `;
--- components/Header2/index.jsx
+++ components/Header2/index.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { getNotifications } from '../../service.js';
 import {
-    MainContainer,
-    Logo,
-    LeftSide,
-    Title,
-    RightSide,
-    NotificationAccount,
-    Notification,
-    Account
+  MainContainer,
+  Logo,
+  LeftSide,
+  Title,
+  RightSide,
+  NotificationAccount,
+  Notification,
+  Account,
+  Screen,
+  NavigationContainer,
+  DropDownContainer
 } from './HeaderStyle';
 import QuestionTooltip from '../../components2/QuestionTooltip';
 import questionMark from '../../static/img/question-mark.svg';
@@ -16,40 +20,151 @@ import addBtnIcon from '../../static/icons/gradient_add.svg';
 import notificationsIcon from '../../static/icons/notifications.svg';
 import accountIcon from '../../static/icons/mask-group-4@2x.png';
 import RoundedBorderLeftIconBtn from '../../components2/Buttons/RoundedBorderLeftIconBtn';
+import { useQuery } from '@tanstack/react-query';
+import NotificationsBar from '../NotificationsMenu/NotificationsBar/index.jsx';
+import ProfileDropdown from '../ProfileMenu/ProfileDropdown/index.jsx';
 
 const Header = () => {
-    const history = useHistory();
+  const [isNotificationOpen, setIsNotificationOpen] = useState(false);
+  const [slideNotificationBar, setSlideNotificationBar] = useState(false);
+  const [dropDown, setDropDown] = React.useState(false);
+  const [pageHeight, setPageHeight] = useState(0);
+  const [sliderOpen, setsliderOpen] = useState(false);
+  const [fixedTop, setfixedTop] = useState(false);
+  const notificationBarRef = useRef(null);
+  const history = useHistory();
 
-    const handleNewTaskClick = () => {
-        history.push('/tasks/new');
+  const { data: notifications, isLoading } = useQuery({
+    queryKey: ['notifications'],
+    queryFn: async () => {
+      const result = await getNotifications();
+      return result;
+    },
+    staleTime: 60000,
+  });
+
+  const handleNewTaskClick = () => {
+    history.push('/tasks/new');
+  };
+
+  const toggleDropDown = () => {
+    setDropDown(!dropDown);
+    setIsNotificationOpen(false);
+  };
+
+  const handleNotificationClick = () => {
+    if (!isNotificationOpen) {
+      setIsNotificationOpen(true);
+      setSlideNotificationBar(true);
+      setsliderOpen(true);
+    } else {
+      setSlideNotificationBar(false);
+      setsliderOpen(false);
+      setTimeout(() => {
+        setIsNotificationOpen(false);
+      }, 300);
+    }
+  };
+
+  useEffect(() => {
+    const updateHeight = () => {
+      const fullHeight = document.documentElement.scrollHeight;
+      if (fullHeight !== pageHeight) {
+        setPageHeight(fullHeight - 170);
+      }
     };
 
+    const observer = new MutationObserver((mutations) => {
+      let shouldUpdateHeight = false;
+
+      for (const mutation of mutations) {
+        if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {
+          shouldUpdateHeight = true;
+          break;
+        }
+      }
+
+      if (shouldUpdateHeight) {
+        updateHeight();
+      }
+    });
+
+    const config = { childList: true, subtree: true };
+
+    observer.observe(document.body, config);
+
+    updateHeight();
+
+    return () => observer.disconnect();
+  }, []);
+
   return (
-    <MainContainer>
+    <div
+      ref={notificationBarRef}
+      style={{
+        position: 'sticky',
+        top: '0',
+        zIndex: '5',
+      }}
+    >
+      <MainContainer>
         <LeftSide>
-            <Logo src="./img/logo.svg" /> 
-            <Title>
-                Tasks
-                <QuestionTooltip 
-                   img={questionMark}
-                   text="View the status of every task that you have assigned for your classes"
-                />
-            </Title>
+          <Logo src="./img/logo.svg" />
+          <Title>
+            Tasks
+            <QuestionTooltip
+              img={questionMark}
+              text="View the status of every task that you have assigned for your classes"
+            />
+          </Title>
         </LeftSide>
         <RightSide>
-            {/* <Buttons link="#tasks/new" /> */}
-            <RoundedBorderLeftIconBtn 
-               onclick={handleNewTaskClick}
-               leftIcon={addBtnIcon}
-               btnText="New Task"
+          {/* <Buttons link="#tasks/new" /> */}
+          <RoundedBorderLeftIconBtn
+            onclick={handleNewTaskClick}
+            leftIcon={addBtnIcon}
+            btnText="New Task"
+          />
+          <NotificationAccount>
+            <Notification
+              src={notificationsIcon}
+              onClick={handleNotificationClick}
             />
-            <NotificationAccount>
-                <Notification src={notificationsIcon} />
-                <Account src={accountIcon} />
-            </NotificationAccount>
+            <Account src={accountIcon} onClick={toggleDropDown} />
+          </NotificationAccount>
         </RightSide>
-    </MainContainer>
-  )
-}
+        {isNotificationOpen && (
+          <Screen
+            onClick={handleNotificationClick}
+            notifications={notifications}
+            pageHeight={pageHeight}
+          >
+            <NavigationContainer
+              slideNotificationBar={sliderOpen}
+              pageHeight={pageHeight}
+              onClick={(e) => e.stopPropagation()}
+              ref={notificationBarRef}
+              fixedTop={fixedTop}
+            >
+              {' '}
+              <NotificationsBar
+                notifications={notifications}
+                loadingNotifications={isLoading}
+                fixedTop={fixedTop}
+              />{' '}
+            </NavigationContainer>
+          </Screen>
+        )}
+        {dropDown && (
+          <Screen onClick={toggleDropDown} pageHeight={pageHeight}>
+            <DropDownContainer>
+              <ProfileDropdown />
+            </DropDownContainer>
+          </Screen>
+        )}
+      </MainContainer>
+    </div>
+  );
+};
 
-export default Header
+export default Header;

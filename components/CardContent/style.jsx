bec743d1cf53452cb85572a2a07de15693fa9117
Mahmudul Hasan <mahmudul@pureblocks.com.au>
2024-03-21 15:03:49 +0600

    fixed the feedback comment word overlapping

--- components/CardContent/style.jsx
+++ components/CardContent/style.jsx
@@ -73,6 +73,7 @@ export const SmallTaskTitle = styled.p`
   align-self: stretch;
   margin: 3px 0;
   letter-spacing: 0;
+  word-break: break-word;
 `;
 export const ClassText = styled.div`
   ${IbmplexsansNormalRiverBed14px}

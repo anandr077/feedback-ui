29bb67f97d5a3f41c9e0eae40ec38d98504f36f2
Mahmudul Hasan <mahmudul@pureblocks.com.au>
2024-06-09 18:27:57 +0600

    made the overall feedback shows only if available

--- components/Header2/rules.js
+++ components/Header2/rules.js
@@ -3,6 +3,5 @@ export const isTeacherWithClass = (role, localClasses) =>{
 }
 
 export const isTeacherWithoutClass = (role, localClasses) =>{
-    console.log('the local classes is', localClasses)
     return role === 'TEACHER' && localClasses == null;
 }

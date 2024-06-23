70ce9b4ff4f4d67e9df70679037adc96847bebd4
Aman Manocha <amanmanocha@yahoo.com>
2024-06-24 01:21:09 +1000

    Frequent fetch JeddAI

--- components/FeedbacksComponents/FeedbacksRoot/index.jsx
+++ components/FeedbacksComponents/FeedbacksRoot/index.jsx
@@ -1442,8 +1442,8 @@ export default function FeedbacksRoot({ isDocumentPage }) {
       }
 
       setTimeout(() => {
-        interval = setInterval(getAndUpdateSubmission, 30000);
-      }, 30000);
+        interval = setInterval(getAndUpdateSubmission, 10000);
+      }, 10000);
     });
   };
 

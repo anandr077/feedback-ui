377e2653639cbe38e560c286b13102f376255e9b
Manoj1408 <manojkumarreddy@alumni.iitm.ac.in>
2024-06-28 22:58:48 +0530

    feedback page arrow

---
+++ components/FeedbacksComponents/CriteriaAndOverallFeedback/ToggleArrow.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import { HideArrow } from './style';
+import ArrowDownIcon from '../../../static/img/arrowdown2.svg';
+import ArrowUpIcon from '../../../static/img/arrowup2.svg';
+
+function ToggleArrow({ refProp, toggleSection }) {
+  const [isVisible, setIsVisible] = useState(false);
+  const toggleVisibility = () => {
+    setIsVisible(!isVisible);
+    toggleSection(refProp);
+  };
+  return (
+    <HideArrow
+      src={isVisible ? ArrowUpIcon : ArrowDownIcon}
+      onClick={toggleVisibility}
+    />
+  );
+}
+
+export default ToggleArrow;

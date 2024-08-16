a51aa565f6b815a897f014ea2ace6bade8c37216
Manoj1408 <manojkumarreddy@alumni.iitm.ac.in>
2024-08-16 18:59:18 +0530

    five stages added

--- components/FeedbacksComponents/SubmissionAITab/index.jsx
+++ components/FeedbacksComponents/SubmissionAITab/index.jsx
@@ -10,41 +10,68 @@ import {
 import RightSidebarHeading from '../RightSidebarHeading';
 import '@lottiefiles/lottie-player';
 import AIFeedbackImage from '../../../static/img/AI-feedback-Image.svg';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
+const lottieAnimations = [
+  { src: './icons/Scanning.json', text: 'Scanning the document' },
+  { src: './icons/Reviewing.json', text: 'Reviewing the document' },
+  { src: './icons/Commenting.json', text: 'Commenting on the document' },
+  { src: './icons/Grading.json', text: 'Grading the document' },
+  { src: './icons/Finaliasing.json', text: 'Finalising the document' },
+];
 
+const SubmissionAITab = ({ handleClose, openRightPanel, showLottie }) => {
+  const [currentAnimationIndex, setCurrentAnimationIndex] = useState(0);
 
-const SubmissionAITab = ({ handleClose, openRightPanel ,showLottie}) => {
-  
+  useEffect(() => {
+    if (showLottie && currentAnimationIndex < lottieAnimations.length - 1) {
+      const intervalId = setInterval(() => {
+        setCurrentAnimationIndex((prevIndex) => prevIndex + 1);
+      }, 4000);
+
+      return () => clearInterval(intervalId);
+    }
+  }, [showLottie, currentAnimationIndex]);
+
+  const { src, text } = lottieAnimations[currentAnimationIndex];
+
   const active = true;
   return (
     <MainContainer openRightPanel={openRightPanel}>
-      <RightSidebarHeading
-        title={'JeddAI Review'}
-        handleClose={handleClose}
-      />
+      <RightSidebarHeading title={'JeddAI Review'} handleClose={handleClose} />
       <SubmissionBody>
-        {showLottie ? 
-        <LottieContainer>
-        <lottie-player
-          autoplay
-          loop
-          mode="normal"
-          src="./icons/Scanning.json"
-        ></lottie-player>
-         <StyledText>Scanning the document</StyledText>
-        </LottieContainer> :
-         <>
-        <AIImage src={AIFeedbackImage} alt="AI Feedback Image" />
-        <TextContainer>
-          <ul>
-            <li>Sorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
-              <li>Nunc vulputate libero et velit interdum, ac aliquet odio mattis.
-              </li>
-              <li>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</li>
-          </ul>
-        </TextContainer>
-        </>}
-     
+        {showLottie ? (
+          <LottieContainer>
+            <lottie-player
+              key={src}
+              autoplay
+              loop
+              mode="normal"
+              src={src}
+            ></lottie-player>
+            <StyledText>{text}</StyledText>
+          </LottieContainer>
+        ) : (
+          <>
+            <AIImage src={AIFeedbackImage} alt="AI Feedback Image" />
+            <TextContainer>
+              <ul>
+                <li>
+                  Sorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </li>
+                <li>
+                  Nunc vulputate libero et velit interdum, ac aliquet odio
+                  mattis.
+                </li>
+                <li>
+                  Class aptent taciti sociosqu ad litora torquent per conubia
+                  nostra, per inceptos himenaeos.
+                </li>
+              </ul>
+            </TextContainer>
+          </>
+        )}
       </SubmissionBody>
     </MainContainer>
   );

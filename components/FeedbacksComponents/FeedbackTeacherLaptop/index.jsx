855389d782448e3a3662f1fa74d1ec194b9fd16b
Mahmudul Hasan <mahmudul@pureblocks.com.au>
2024-10-23 19:21:33 +0600

    ai sidebar will stay open by default based on some condition

--- components/FeedbacksComponents/FeedbackTeacherLaptop/index.jsx
+++ components/FeedbacksComponents/FeedbackTeacherLaptop/index.jsx
@@ -140,9 +140,18 @@ function FeedbackTeacherLaptop(props) {
       (commentBank) => commentBank.id === currentCommentBankId
   ));
 
-  useEffect(() => {
-    setOpenRightPanel(isShowMarkingCriteriaSidebar(overallComments, markingCriteriaFeedback) ? 'tab2' : null)
-  },[])
+  useEffect(()=>{
+    const shouldShowMarkingCriteriaSidebar = isShowMarkingCriteriaSidebar(overallComments, markingCriteriaFeedback);
+    const shouldShowAiFeedbackSidebar = showAiFeedbackGenerateButton(submission.type, pageMode, isTeacher, submission.assignment.reviewedBy);
+      
+    if (shouldShowMarkingCriteriaSidebar) {
+        setOpenRightPanel('tab2');
+      } else if (shouldShowAiFeedbackSidebar) {
+        setOpenRightPanel('tab4');
+      } else {
+        setOpenRightPanel(null);
+      }
+  }, [])
 
   useEffect(() => {
     if (showNewComment) {

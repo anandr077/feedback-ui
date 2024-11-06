d2da038bd51c5ca6c081fe6cb126b23bb1685112
Aman Manocha <amanocha@seek.com.au>
2024-11-06 12:25:23 +1100

    Revert "Revert "Revert "Develop"""

--- rules.js
+++ rules.js
@@ -1,11 +1,5 @@
-import { isNullOrEmpty } from "./utils/arrays";
-
 export const shouldShowComponent = (routesToHide) => {
     const currentHash = window.location.hash.split('?')[0];
   return !routesToHide.some((route) => currentHash.startsWith(route));
-};
-
-export const isTeacherWithoutClass = (role, localClasses) => {
-  return role === 'TEACHER' && isNullOrEmpty(localClasses);
 };
 

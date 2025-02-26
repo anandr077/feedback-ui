09d32d516d4636b3ca29830cf177b3f6ec9a47ba
Mahmudul Hasan <mahmudul@pureblocks.com.au>
2025-02-26 10:37:53 +0600

    Teacher task dropdown item shown with condition

--- components/TaskCard/rules.js
+++ components/TaskCard/rules.js
@@ -1,7 +1,19 @@
+import { getUserId } from "../../userLocalDetails";
+
+ const userId = getUserId();
+
 export const isShowProgressBar = (task) => {
     return task.status != 'DRAFT' && task.submissionCount >= 0;
 }
 
 export const isShowShareOption = (taskStatus) => {
     return taskStatus === 'PUBLISHED'
+}
+
+export const isShowChangeDueTime = (teacherId) =>{
+    return userId === teacherId
+}
+
+export const isShowDeleteOption = (teacherId) =>{
+    return userId === teacherId 
 }

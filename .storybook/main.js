6f9b8ee0bfa5f82ea563c3e03de48121ff9c97b0
Mahmudul Hasan <mahmudul@pureblocks.com.au>
2025-02-26 13:36:46 +0600

    Updated the branch

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,19 +1,26 @@
+
+
 /** @type { import('@storybook/react-webpack5').StorybookConfig } */
 const config = {
-  stories: [
-    '../stories/**/*.mdx',
-    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+  "stories": [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
-  addons: [
-    '@storybook/addon-webpack5-compiler-swc',
-    '@storybook/addon-onboarding',
-    '@storybook/addon-essentials',
-    '@chromatic-com/storybook',
-    '@storybook/addon-interactions',
+  "addons": [
+    "@storybook/addon-webpack5-compiler-swc",
+    {
+      "name": "@storybook/addon-essentials",
+      "options": {
+        "docs": false
+      }
+    },
+    "@storybook/addon-onboarding",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions"
   ],
-  framework: {
-    name: '@storybook/react-webpack5',
-    options: {},
-  },
+  "framework": {
+    "name": "@storybook/react-webpack5",
+    "options": {}
+  }
 };
 export default config;

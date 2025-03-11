891216b70e55646b2fb9b2c49af96ed5e6219fde
anand008186 <anand_r@mt.iitr.ac.in>
2025-03-11 23:54:37 +0530

    Revert "integrated welcome onboarding api"
    This reverts commit ecd2ba0e4974e4fe074e742e3ff3ef6390a4c598.

--- index.js
+++ index.js
@@ -3,27 +3,5 @@ import ReactDOM from 'react-dom';
 import './globals.css';
 import './styleguide.css';
 import App from './App';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-const client = new QueryClient({
-    // defaultOptions: {
-    //   queries: {
-    //     retry: (failureCount, error) => {
-    //       console.log('retry error', error);
-    //       return !(error.status === 401);  // Stop retries if status is 401
-    //     },
-    //     onError: (error) => {
-    //       console.log('error', error);
-    //       if (error.status === 401) {
-    //         handleRedirect();
-    //       }
-    //     },
-    //   },
-    // },
-  });
 
-ReactDOM.render(
-  <QueryClientProvider client={client}>
-    <App /> 
-  </QueryClientProvider>,
-  document.getElementById('app')
-);
+ReactDOM.render(<App />, document.getElementById('app'));
